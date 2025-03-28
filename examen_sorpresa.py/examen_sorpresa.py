4670d43c59e056f6485016f537bdeff02564be33
jhackisneros <oliveracisneros1@gmail.com>
2025-03-28 08:19:55 +0000

    cambios

---
+++ examen_sorpresa.py/examen_sorpresa.py
@@ -0,0 +1,8 @@
+class punto():
+    def __init__(sef,x=0,y=0):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        return "({},{})".format(self.x,self.y)
+                                                                                                          
+    
