4bfdd37690f7a9ecaa602e0ea8bee41d8f15697d
jhackisneros <oliveracisneros1@gmail.com>
2025-03-28 08:47:00 +0000

    cambios

---
+++ class_rectangulo.py/rectangulo.py
@@ -0,0 +1,42 @@
+from class_punto import punto
+
+
+class rectangulo():
+    def __init__(self,punto1=punto(),punto2=punto()):
+        super().__init__()
+        self.punto1 = punto1
+        self.punto2 = punto2
+    def base(self):
+        return "la base del rectangulo es{}".format(abs(self.punto1.x-self.punto2.x))
+    def altura(self):
+        return "la altura del rectangulo es{}".format(abs(self.punto1.y-self.punto2.y))
+    def area(self):
+        return "el area del rectangulo es{}".format(self.base()*self.altura())
+    
+a = punto(4,8)
+b = punto(9,9)
+c = punto (-5,-10)
+d = punto(0,0)
+    
+print("los puntos son")
+print(f"el punto a es {a}")
+print(f"el punto b es {b}")
+print(f"el punto c es {c}")
+print(f"el punto d es {d}")
+
+print("\n vectores")
+print(f"el vector ab es {a.vector(b)}")
+print(f"el vector ba es {b.vector(a)}")
+
+print("\n Distancias")
+print(f"la distancia entre ab es {a.distancia(b)}")
+print(f"la distancia entre ba es {b.distancia(a)}")
+
+distancias = {"a":a.distancia(d),"b":b.distancia(d),"c":c.distancia(d)}
+punto_mas_lejano = max(distancias,key=distancias.get)
+print(f"el punto mas lejano es {punto_mas_lejano} y su distancia es {distancias[punto_mas_lejano]}")
+rectangulo = rectangulo(a,b)
+print("\nRectangulo")
+print(f"la base es {rectangulo.base}()")
+print(f"la altura es {rectangulo.altura}()")
+print(f"el area es {rectangulo.area}()")
