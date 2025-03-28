0a0a3bed8a2a404e2baa90e28cc11ae8e7ccae99
jhackisneros <oliveracisneros1@gmail.com>
2025-03-28 08:36:15 +0000

    cambios

--- examen_sorpresa.py/examen_sorpresa.py
+++ examen_sorpresa.py/examen_sorpresa.py
@@ -55,5 +55,9 @@ print("\n Distancias")
 print(f"la distancia entre ab es {a.distancia(b)}")
 print(f"la distancia entre ba es {b.distancia(a)}")
 
+distancias = {"a":a.distancia(d),"b":b.distancia(d),"c":c.distancia(d)}
+punto_mas_lejano = max(distancias,key=distancias.get)
+print(f"el punto mas lejano es {punto_mas_lejano} y su distancia es {distancias[punto_mas_lejano]}")
+
 
                                                                                                           
