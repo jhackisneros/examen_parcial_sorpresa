4cfe9741acb94979ae7e640ad46b0a1fdd971763
jhackisneros <oliveracisneros1@gmail.com>
2025-03-28 09:14:00 +0000

    cambios

--- examen_sorpresa.py/examen_sorpresa.py
+++ examen_sorpresa.py/examen_sorpresa.py
@@ -1,5 +1,3 @@
-import math
-
 class punto():
     def __init__(self,x=0,y=0):
         self.x = x
@@ -35,34 +33,6 @@ class rectangulo():
         return "la altura del rectangulo es{}".format(abs(self.punto1.y-self.punto2.y))
     def area(self):
         return "el area del rectangulo es{}".format(self.base()*self.altura())
-    
-a = punto(4,8)
-b = punto(9,9)
-c = punto (-5,-10)
-d = punto(0,0)
-    
-print("los puntos son")
-print(f"el punto a es {a}")
-print(f"el punto b es {b}")
-print(f"el punto c es {c}")
-print(f"el punto d es {d}")
-
-print("\n vectores")
-print(f"el vector ab es {a.vector(b)}")
-print(f"el vector ba es {b.vector(a)}")
-
-print("\n Distancias")
-print(f"la distancia entre ab es {a.distancia(b)}")
-print(f"la distancia entre ba es {b.distancia(a)}")
-
-distancias = {"a":a.distancia(d),"b":b.distancia(d),"c":c.distancia(d)}
-punto_mas_lejano = max(distancias,key=distancias.get)
-print(f"el punto mas lejano es {punto_mas_lejano} y su distancia es {distancias[punto_mas_lejano]}")
-rectangulo = rectangulo(a,b)
-print("\nRectangulo")
-print(f"la base es {rectangulo.base}()")
-print(f"la altura es {rectangulo.altura}()")
-print(f"el area es {rectangulo.area}()")
 
 
                                                                                                           
